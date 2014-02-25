9f9be07df3a202095052012714d10eaa2040a012
cenderin <ml.alexander87@gmail.com>
2014-02-24 16:15:58 -0800

    First commit for inventory app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iphonepartinventory::Application.config.secret_key_base = 'e7db143a52262f426f2fa49372477141d76f2bffdc14e1602a4f11716cda198af2cdce345e7aeaea2a0525b85902f6718404ce9be399c4131f36444917646b6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iphonepartinventory::Application.config.session_store :cookie_store, key: '_iphonepartinventory_session'
